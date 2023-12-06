0c0eda6d33de8db1b5e9aebd64d1354744d5efbd
pranesh12 <praneshchakama6@gmail.com>
2023-12-06 23:57:12 +0600

    signup form added

--- src/features/auth/RegisterView.jsx
+++ src/features/auth/RegisterView.jsx
@@ -1,9 +1,53 @@
 import React from "react";
 
 const RegisterView = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("working");
+  };
+
   return (
-    <div>
-      <h1>Register view</h1>
+    <div className="container mx-auto mt-5 prose">
+      <div className="flex">
+        <div className="basis-1/2"></div>
+        <div className="basis-1/2">
+          <h2>New Account</h2>
+          <p>Already have an account?</p>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="fistname"
+              placeholder="First Name"
+              className="input input-bordered w-full max-w-xs  mb-4  input-info"
+            />
+            <input
+              type="text"
+              name="lastname"
+              placeholder="Last Name"
+              className="input input-bordered w-full max-w-xs mb-4 input-info"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="input input-bordered w-full max-w-xs mb-4 input-info"
+            />
+            <input
+              type="password"
+              name="email"
+              placeholder="Password"
+              className="input input-bordered w-full max-w-xs mb-4 input-info"
+            />
+
+            <button
+              type="submit"
+              className="btn btn-wide btn-outline btn-success"
+            >
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
